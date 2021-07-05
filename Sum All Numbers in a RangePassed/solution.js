2b3ff85b47ae9253037d8d579911a57a7b4a60c3
Avinash Dhumal <avinaashdhumal@gmail.com>
2021-07-05 12:02:13 +0530

    Added discription to the Q1. Sum All Numbers

--- Sum All Numbers in a RangePassed/solution.js
+++ Sum All Numbers in a RangePassed/solution.js
@@ -1,15 +1,21 @@
 function sumAll(arr) {
+    // create block level variable with 'let' keyword and give an initial value 0
     let sum = 0
+    // If the first value of array is greater than the second value 
     if (arr[0] > arr[1]) {
+        // create a range between first value to the last value using for loop
         for (let i = arr[1]; i <= arr[0]; i++) {
+            // sum all the numbers between the range
             sum += i
         }
     }
+    // if second value is greater than the first value of the array
     else {
         for (let i = arr[0]; i <= arr[1]; i++) {
             sum += i
         }
     }
+    // finally return the sum all the numbers
     return sum
 }
 
